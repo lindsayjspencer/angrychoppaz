a807218f6ef3dd10ca53d5b8f7cb1c7a4c58b82d
Lindsay <lindsayjspencer@gmail.com>
2020-05-10 19:45:08 +1000

    .

--- bin/index.js
+++ bin/index.js
@@ -8,67 +8,69 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question("Enter port: (3000 is default)", function(port) {
-    if(port=='') {
-        var port = process.env.PORT || 3000;
+rl.question("Enter port: (3000 is default)\n", function(enteredport) {
+    if (enteredport == '') {
+        port = process.env.PORT || 3000;
+    } else {
+        port = enteredport;
     }
+    rl.close();
 });
 
 rl.on("close", function() {
-    console.log("\nBYE BYE !!!");
-    process.exit(0);
-});
 
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http, {
-  pingInterval: 1000,
-  pingTimeout: 3000
-});
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-console.log("//////////////////////////////////////////////////////////////////////////////////////");
-console.log("Angry Choppers has begun");
-console.log("Starting http server on port "+port);
-console.log("//////////////////////////////////////////////////////////////////////////////////////");
-
-
-app.use(express.static('js'))
-app.use(express.static('img'))
-app.use(express.static('node_modules'))
-
-var allClients = [];
-var allIds = [];
-io.sockets.on('connection', function(socket) {
-
-    socket.on('sendname', function(name) {
-        socket.broadcast.emit('new player name', name);
-    });
-    socket.broadcast.emit('new player', socket.id);
-    allClients.push(socket);
-    socket.emit('self id', socket.id );
-    socket.emit('player list', allIds );
-    allIds.push(socket.id);
-    console.log(Math.floor(Date.now()/1000)+": New player connected");
-    console.log(Math.floor(Date.now()/1000)+": "+allClients.length+" players");
-    socket.on('chat message', function(msg){
-        socket.broadcast.emit('chat message', msg);
-    });
-    socket.on('disconnect', function() {
-        socket.broadcast.emit('user disconnect', socket.id);
-        console.log(Math.floor(Date.now()/1000)+": "+'Player disconnected');
-        var i = allClients.indexOf(socket);
-        allClients.splice(i, 1);
-        allIds.splice(i, 1);
-        console.log(Math.floor(Date.now()/1000)+": "+allClients.length+" players");
+    var express = require('express');
+    var app = express();
+    var http = require('http').Server(app);
+    var io = require('socket.io')(http, {
+        pingInterval: 1000,
+        pingTimeout: 3000
     });
 
-});
+    app.get('/', function(req, res) {
+        res.sendFile(__dirname + '/index.html');
+    });
 
-http.listen(port, function(){
-  console.log(Math.floor(Date.now()/1000)+": "+'listening on *:' + port);
+    console.log("///////////////////////////////////////////////////////////////////////\n\n");
+    console.log("Initialising Angry Choppaz");
+    console.log("Starting http server on port " + port + "\n\n");
+    console.log("///////////////////////////////////////////////////////////////////////\n");
+
+
+    app.use(express.static('js'))
+    app.use(express.static('img'))
+    app.use(express.static('node_modules'))
+
+    var allClients = [];
+    var allIds = [];
+    io.sockets.on('connection', function(socket) {
+
+        socket.on('sendname', function(name) {
+            socket.broadcast.emit('new player name', name);
+        });
+        socket.broadcast.emit('new player', socket.id);
+        allClients.push(socket);
+        socket.emit('self id', socket.id);
+        socket.emit('player list', allIds);
+        allIds.push(socket.id);
+        console.log(Math.floor(Date.now() / 1000) + ": New player connected");
+        console.log(Math.floor(Date.now() / 1000) + ": " + allClients.length + " players");
+        socket.on('chat message', function(msg) {
+            socket.broadcast.emit('chat message', msg);
+        });
+        socket.on('disconnect', function() {
+            socket.broadcast.emit('user disconnect', socket.id);
+            console.log(Math.floor(Date.now() / 1000) + ": " + 'Player disconnected');
+            var i = allClients.indexOf(socket);
+            allClients.splice(i, 1);
+            allIds.splice(i, 1);
+            console.log(Math.floor(Date.now() / 1000) + ": " + allClients.length + " players");
+        });
+
+    });
+
+    http.listen(port, function() {
+        console.log(Math.floor(Date.now() / 1000) + ": " + 'listening on *:' + port);
+    });
+
 });
